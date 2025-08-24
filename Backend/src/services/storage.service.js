0d080cb05bf5ff7ffa19c701c00c819c523bf5f9
alokishere <alokvishwakarmabdh@gmail.com>
2025-08-24 17:40:50 +0530

    80% done

---
+++ Backend/src/services/storage.service.js
@@ -0,0 +1,21 @@
+var ImageKit = require("imagekit");
+
+var imagekit = new ImageKit({
+    publicKey : process.env.IMAGEKIT_PUBLIC_URL,
+    privateKey : process.env.IMAGEKIT_PRIVATE_URL,
+    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
+});
+
+function uploadFile(file){
+return new Promise((resolve,reject)=>{
+    imagekit.upload({
+        file : file.buffer,
+        fileName : "test.jpg"
+    },(error,result)=>{
+        if(error) return reject(error);
+        resolve(result);
+    })
+})
+}
+
+module.exports = uploadFile;
